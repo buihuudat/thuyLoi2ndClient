ff473610fd648116eab68f7924f3ba93b129daa5
tran thanh tu <tttuab@gmail.com>
2023-03-12 23:48:53 +0700

    update

--- components/PostProductItem.js
+++ components/PostProductItem.js
@@ -1,5 +1,3 @@
-<<<<<<< HEAD
-//tran thanh tu
 import React from "react";
 import { Image, StyleSheet, Text, TouchableOpacity, View } from "react-native";
 import IonIcons from "react-native-vector-icons/Ionicons";
@@ -33,40 +31,7 @@ const PostProductItem = ({ postproduct, navigate }) => {
         </View>
       </View>
     </TouchableOpacity>
-=======
-import React from "react";
-import { Image, StyleSheet, Text, TouchableOpacity, View } from "react-native";
-import IonIcons from "react-native-vector-icons/Ionicons";
-import Colors from "../components/contants/Colors";
 
-const PostProductItem = ({ postproduct }) => {
-  return (
-    <View style={styles.container}>
-      <View>
-        <View style={styles.bookmarkIcon}>
-          <IonIcons
-            name="bookmark-outline"
-            size={23}
-            style={{ color: Colors.DEFAULT_RED }}
-          />
-        </View>
-        <TouchableOpacity>
-          <Image style={styles.image} source={postproduct.source} />
-        </TouchableOpacity>
-
-        <View style={styles.titlePostProduct}>
-          <Text style={styles.titleContent}>{postproduct.title}</Text>
-          <IonIcons name="ellipsis-vertical-outline" size={19} />
-        </View>
-
-        <Text style={styles.price}>{postproduct.price} đ</Text>
-        <View style={styles.timeContainer}>
-          <IonIcons name="time" size={14} />
-          <Text style={styles.time}>{postproduct.time}</Text>
-        </View>
-      </View>
-    </View>
->>>>>>> 7a5901ca5a2d972bc59bdb265c40b25ce4ee7b43
   );
 };
 
